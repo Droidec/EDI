65cad43a0e120436fcfe65b72daa0588ee0e341f
Droidec <derovor@gmail.com>
2022-02-07 22:23:56 +0100

    Fix misc

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -34,4 +34,4 @@ EDI cogs (collection of commands and listeners)
 from .err import CogErrHandler
 from .basic import CogBasic
 from .voice import CogVoice
-from .plex import CogPlex
+from .plex import CogPlexServer
